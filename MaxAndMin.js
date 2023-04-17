595098c5938854cb77a8e374a9f932aa7751016f
fadyjan <fady.jan95@gmail.com>
2023-04-17 13:19:20 +0200

    //8Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.

--- MaxAndMin.js
+++ MaxAndMin.js
@@ -1,10 +1,10 @@
-debugger
+debugger;
 
 //1 Get Min Value using OneLine
 /* var arr = [10,20,30,-2,-30,0]
 console.log(Math.min(...arr))*/
 
-//2 Get Min Value from Array Long Solution 
+//2 Get Min Value from Array Long Solution
 /* function min(Arr) {
   var minVal = 0 ;
   for (let x = 0; x < Arr.length-1; x++) {
@@ -21,8 +21,7 @@ console.log (min([-10, -20, -30, 0, 20]))
 /*let arr = [10,20,30,0,-40]
 console.log(Math.max(...arr))*/
 
-
-//4 get max value from array Long Soultion 
+//4 get max value from array Long Soultion
 /* function max (Arr){
     var maxval = 0 ;
     for(var x = 0 ; x<Arr.length;x++){
@@ -34,14 +33,12 @@ console.log(Math.max(...arr))*/
 }
  console.log(max([10,20,-20,0,99]))*/
 
-
 //5 Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 
 /*let NameAsString = 'Sam Harris'
 let NameAsArray = NameAsString.split(" ")
 let initialName = NameAsArray[0][0].toUpperCase() + "." + NameAsArray[1][0].toUpperCase()
 console.log(initialName)*/
-
 
 //6 This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 /*var num = 7
@@ -53,3 +50,17 @@ if (num % 2 === 0) {
     num = num *9
 }*/
 
+//8Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+/*function invert(array) {
+  invertedArray = array.map((el) => {
+    if (el != 0) {
+      return el * -1;
+    } else {
+      return 0;
+    }
+  });
+  return invertedArray;
+}
+
+console.log(invert([]));
+*/

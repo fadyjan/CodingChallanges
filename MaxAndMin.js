c09c6dcc7a91191c22345540fd56d4a8b1e40d06
fadyjan <fady.jan95@gmail.com>
2023-04-20 00:08:01 +0200

    //17 We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.

--- MaxAndMin.js
+++ MaxAndMin.js
@@ -172,3 +172,20 @@ console.log(updateLight("green")) */
 
 
 console.log(summation(8))  */
+//17 We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+function points(games) {
+  var TotalPoints =  0;
+  games.forEach(game => {
+      let match = game.split(':')
+      if (  match[0] > match[1]) {
+        TotalPoints += 3
+      } else if(  match[0] == match[1]) {
+        TotalPoints += 1
+      }
+    
+  });
+  return TotalPoints
+}
+
+console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]))

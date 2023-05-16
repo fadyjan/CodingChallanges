8cb7364d219b9089b1afebf5d0d7d6254b33c2db
fadyjan <fady.jan95@gmail.com>
2023-05-16 20:58:37 +0300

    //35 implement HashTable

--- CodingChallanges.js
+++ CodingChallanges.js
@@ -637,14 +637,70 @@ pigIt('Pig latin is cool') */
 
 //34 Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.Note that you must do this in-place without making a copy of the array.
 
-var moveZeroes = function(nums) {
-    debugger
-  for (let index = 0; index < nums.length; index++) {
-    if (nums[index] ===0 ){
-      nums.splice(index,1)
-      nums.push(0)
-    }
-  }
-  return nums
-};
-console.log(moveZeroes([0,1,0,3,12]))
+// var moveZeroes = function(nums) {
+//     debugger
+//   for (let index = 0; index < nums.length; index++) {
+//     if (nums[index] ===0 ){
+//       nums.splice(index,1)
+//       nums.push(0)
+//     }
+//   }
+//   return nums
+// };
+// console.log(moveZeroes([0,1,0,3,12]))
+
+//35 implement HashTable
+// class HashTable {
+//   constructor(size){
+//     this.data = new Array(size)
+//   }
+
+//   _hash(key) {
+//     let hash = 0;
+//     for (let i =0; i < key.length; i++){
+//         hash = (hash + key.charCodeAt(i) * i) % this.data.length
+//     }
+//     return hash;
+//   }
+
+//   set(key, value) {
+//     let address = this._hash(key);
+//     if (!this.data[address]) {
+//       this.data[address] = [];
+//     }
+//     this.data[address].push([key, value]);
+//     return this.data;
+//   }
+
+//   get(key){
+//     const address = this._hash(key);
+//     const currentBucket = this.data[address]
+//     if (currentBucket) {
+//       for(let i = 0; i < currentBucket.length; i++){
+//         if(currentBucket[i][0] === key) {
+//           return currentBucket[i][1]
+//         }
+//       }
+//     }
+//     return undefined;
+//   }
+
+//   keys(){
+//     debugger
+//     const KeysArray =[]
+//     for (let index = 0; index < this.data.length; index++) {
+//       if ((this.data[index])) {
+//         KeysArray.push(this.data[index][0][0]) 
+
+//       }
+      
+//     }
+//     return KeysArray
+//   }
+
+// }
+
+// const h1 = new HashTable(50)
+// h1.set('grappes',1000)
+// h1.set('aples',2000)
+// console.log(h1.keys())
